0da5f35810434ab8fae1f12f32600a73f0fedc45
Priyanka <priyankaraju22@gmail.com>
2019-11-05 20:21:49 +0530

    Update settings

--- capstone2nd/settings.py
+++ capstone2nd/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 
 
-# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +129,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+django_heroku.settings(locals())
